94a11d3718ea479452f9b62efd381936b75e0a20
kevinchenftw <kevinchenftw@gmail.com>
2021-03-05 17:51:15 -0600

    for you kev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,20 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Contractors from "./gov-models/Contractors";
 class App extends Component {
-  state = {
-    topics: ["Hello..."],
-    question: "",
-    answer: ""
-  }
-
-  componentDidMount() {
-    this.fetchTopics()
-
-  }
-
-  fetchTopics = async () => {
-    const { data } = await axios.get(
-      `${process.env.REACT_APP_API_URL}/get_topics`,
-    );
-    const { topics } = data;
-    this.setState({topics})
-  }
-
-  handleChange = (event) => {
-    this.setState({question: event.target.value});
-  }
-
-  handleSubmit = (event) => {
-    this.fetchAnswer();
-    event.preventDefault();
-  }
-
-  fetchAnswer = async () => {
-    const { question } = this.state;
-    const { data } = await axios.post(
-      `${process.env.REACT_APP_API_URL}/submit_question`, { question }
-    );
-    const { answer } = data;
-    this.setState({answer})
-  }
-
   render() {
-    const { topics, question, answer } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-        <h1>List of topics to ask a question on</h1>
-        <ul>
-          {topics.map(topic => (<li key={topic}>{topic}</li>))}
-        </ul>
-          <form onSubmit={this.handleSubmit}>
-          <label>
-            Question:
-            <input type="text" value={question} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <h1>Answer: {answer}</h1>
-        </header>
-      </div>
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path = "/Contractors" component = {Contractors} />
+            {/* <Route path = "/gov-models/Contracts" component = {Contracts} />
+            <Route path = "/gov-models/State" component = {State} /> */}
+            {/* <Route component = {NoMatch} /> */}
+          </Switch>
+        </Router>
+      </React.Fragment>
     );
   }
 }
--- frontend/src/gov-models/Contractors.tsx
+++ frontend/src/gov-models/Contractors.tsx
@@ -3,9 +3,11 @@ import React from "react";
 function Contractors(){
     return (
         <div>
+            <h1> HELLO HELLO HELLO</h1>
             <li>Lockheed Martin</li>
             <li>Boeing</li>
             <li>Raytheon</li>
         </div>
-    )
+    );
 }
+export default Contractors
