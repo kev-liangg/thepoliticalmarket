96858d149950dc4632d0ce60e77ef11a58103b45
kevinchenftw <kevinchenftw@gmail.com>
2021-03-05 00:39:35 -0600

    added government models

---
+++ frontend/src/gov-models/contractors.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Contractors(){
+    return (
+        <div>
+            <li>Lockheed Martin</li>
+            <li>Boeing</li>
+            <li>Raytheon</li>
+        </div>
+    )
+}
---
+++ frontend/src/gov-models/contracts.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Contracts(){
+    return (
+        <div>
+            <li>Lockheed Martin</li>
+            <li>Boeing</li>
+            <li>Raytheon</li>
+        </div>
+    )
+}
