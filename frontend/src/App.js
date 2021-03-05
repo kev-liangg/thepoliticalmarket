2a22b973bb26230767ff329723a0eb420ca823aa
kevinchenftw <kevinchenftw@gmail.com>
2021-03-05 16:41:12 -0600

    yarn install with bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class App extends Component {
   state = {
-    topics: ["Loading..."],
+    topics: ["Hello..."],
     question: "",
     answer: ""
   }
---
+++ frontend/src/gov-models/state.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function States(){
+    return (
+        <div>
+            <li>Texas</li>
+            <li>California</li>
+            <li>Virgina</li>
+        </div>
+    )
+}
