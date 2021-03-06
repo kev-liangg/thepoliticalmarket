af6a274f5e9614595a94f7d0ec40e773184c7f13
Kevin Liang <kev.liang@utexas.edu>
2021-03-05 22:32:52 -0600

    implement dynamic api calls to count commits

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -5,7 +5,8 @@ import './App.css';
 class About extends Component {
 
     state = {
-        test: NaN
+        commitVals: [0, 0, 0, 0, 0],
+        issueVals: [0, 0, 0, 0, 0]
     }
 
     async getCommits() {
@@ -14,18 +15,30 @@ class About extends Component {
     }
     
     parseCommits(commits) {
-        console.log(commits);
-        commits.then((value) => console.log(value));
-        let commitVals = [0, 0, 0, 0, 0];
-        for (const c in commits) {
-            console.log(c);
+        let newVals = [0, 0, 0, 0, 0];
+        for (const i in commits) {
+            console.log(commits[i]["author_name"]);
+            switch(commits[i]["author_name"]) {
+                case "Kevin Liang":
+                    ++newVals[0];
+                    break;
+                case "Kevin Chen":
+                case "kevinchenftw":
+                    ++newVals[1];
+                    break;
+                default:
+                    break;
+            }
         }
+        return newVals;
     }
 
     componentDidMount() {
-        let commits = this.getCommits();
-        console.log(commits);
-        commits.then(this.setState({ test: 69 }));
+        const commits = this.getCommits();
+        commits.then((commits) => {
+            console.log(commits);
+            this.setState({commitVals: this.parseCommits(commits)});
+        });
     }
     
     render() {
@@ -40,28 +53,28 @@ class About extends Component {
                     </tr>
                     <tr>
                         <td>Kevin Liang</td>
-                        <td>{this.state.test}</td>
-                        <td></td>
+                        <td>{this.state.commitVals[0]}</td>
+                        <td>{this.state.issueVals[0]}</td>
                     </tr>
                     <tr>
                         <td>Kevin Chen</td>
-                        <td></td>
-                        <td></td>
+                        <td>{this.state.commitVals[1]}</td>
+                        <td>{this.state.issueVals[1]}</td>
                     </tr>
                     <tr>
                         <td>Diyuan Dai</td>
-                        <td></td>
-                        <td></td>
+                        <td>{this.state.commitVals[2]}</td>
+                        <td>{this.state.issueVals[2]}</td>
                     </tr>
                     <tr>
                         <td>Vaishnav Bipin</td>
-                        <td></td>
-                        <td></td>
+                        <td>{this.state.commitVals[3]}</td>
+                        <td>{this.state.issueVals[3]}</td>
                     </tr>
                     <tr>
-                        <td>Anisah Kollareddy</td>
-                        <td></td>
-                        <td></td>
+                        <td>Anisha Kollareddy</td>
+                        <td>{this.state.commitVals[4]}</td>
+                        <td>{this.state.issueVals[4]}</td>
                     </tr>
                     </table>  
               </header>  
