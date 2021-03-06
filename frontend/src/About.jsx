4fb98631fb881a8ecfcace28c81e4a6dc3e704f7
Kevin Liang <kev.liang@utexas.edu>
2021-03-05 20:04:03 -0600

    add basic table rendering

---
+++ frontend/src/About.jsx
@@ -0,0 +1,44 @@
+import './App.css';
+
+function About() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <table>
+                <tr>
+                    <th>Member Name</th>
+                    <th># Commits</th>
+                    <th># Issues</th>
+                </tr>
+                <tr>
+                    <td>Kevin Liang</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Kevin Chen</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Diyuan Dai</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Vaishnav Bipin</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Anisah Kollareddy</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </table>  
+          </header>  
+        </div>
+      );
+}
+
+export default About;
