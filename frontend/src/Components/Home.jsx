14277970d92d8bbe98b18deda65273446cf09c73
Kevin Liang <kev.liang@utexas.edu>
2021-03-06 12:51:04 -0600

    fixed image and title scaling

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import bg from "../1393726.jpg";
-import '../App.css'
+import './Home.css'
 
 class Home extends Component {
 
     render() {
         return (
-            <div>
-            <img src={bg} class="img-fluid" alt="bg" />
+            <div> 
+            <img src={bg} class="img-fluid w-100" alt="bg" />
             <div className="centered">The Political Market</div>
             </div>
         )
