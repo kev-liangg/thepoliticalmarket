31d85a4331c666b473fc34e0a8374b14a8e716ff
VaishnavBipin <vaishnavb88@gmail.com>
2021-03-06 00:37:25 -0600

    update local 2

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Contractors from "./gov-models/Contractors";
+import Government from "./gov-models/Government";
+import {NavigationBar} from "./Components/NavigationBar";
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+        <NavigationBar/>
+          <Switch>
+            <Route exact path = "/Government" component = {Government} />
+            <Route exact path = "/Contractors" component = {Contractors} />
+            {/* <Route path = "/gov-models/Contracts" component = {Contracts} />
+            <Route path = "/gov-models/State" component = {State} /> */}
+            {/* <Route component = {NoMatch} /> */}
+          </Switch>
+        </Router>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
