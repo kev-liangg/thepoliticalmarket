f800009b5550a3f663d29017e8d53228e401b29c
Anisha <bluestar225@gmail.com>
2021-03-06 21:02:08 -0600

    link states to stocks

--- frontend/src/gov-models/State.tsx
+++ frontend/src/gov-models/State.tsx
@@ -4,8 +4,23 @@ function States(){
     return (
         <div>
             <li>Texas</li>
+                <ul>
+                <li>
+                    <a href={"/DELL"}>DELL</a>
+                </li>
+                </ul>
             <li>California</li>
+                <ul>
+                <li>
+                    <a href={"/HPQ"}>HPQ</a>
+                </li>
+                </ul>
             <li>Georgia</li>
+                <ul>
+                <li>
+                    <a href={"/GOOG"}>GOOG</a>
+                </li>
+                </ul>
         </div>
     )
 }
