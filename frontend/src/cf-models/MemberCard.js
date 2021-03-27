afce29a1effb459962940c4b9aa5718847386a6a
VaishnavBipin <vaishnavb88@gmail.com>
2021-03-27 03:27:17 -0500

    cleaning up phase I stuff

--- frontend/src/cf-models/MemberCard.js
+++ frontend/src/cf-models/MemberCard.js
@@ -8,11 +8,15 @@ function MemberCard({member}) {
             <div className='card mt-5'>
             <img className="card-img-top" src={member.cand_image} alt=""></img>
                 <div className='card-body'>
-                <h4 className="card-title">{member.firstname} {member.lastname}</h4>
+                <h4 className="card-title" style={{'textAlign':'center'}}>{member.cand_firstname} {member.cand_lastname}</h4>
                 <p className="card-text">
-                    stuff
+                    OpenSecrets ID: {member.cand_crp_id} <br></br>
+                    Cycle: {member.cycle} <br></br>
+                    Office: {member.cand_office==='s'?'Senate':'House'} <br></br>
+                    Party: {member.cand_party} <br></br>
+                    State: {member.cand_state} <br></br>
                 </p>
-                <Link to={`/CampFin/${member.cand_crp_id}`}>Learn more</Link>
+                <Link to={`/CampFin/${member.cand_crp_id}`}>Learn more...</Link>
                 </div>
             </div>
         </div>
