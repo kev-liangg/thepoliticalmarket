e4f2f9c7d6e6065581b79ecc8f33034937157d51
Kevin Liang <kev.liang@utexas.edu>
2021-03-29 16:27:35 -0500

    add many dependencies for react app testing, success render test

--- frontend/tests.js
+++ frontend/tests.js
@@ -1,3 +1,11 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
 test('successfully run test harness', () => {
     expect(1 + 1).toBe(2)
+});
+
+import App from './src/App'
+test('renders App component', () => {
+    render(<App />);
 });
