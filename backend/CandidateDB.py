1e80418d8090225c45dc9e212e2963a5d85e3f9c
Kevin Liang <kev.liang@utexas.edu>
2021-03-28 22:02:31 -0500

    backend refactor and integration, prep for deploy

--- backend/CandidateDB.py
+++ backend/CandidateDB.py
@@ -1,33 +1,10 @@
-# SQLAlchemy database design class
-import json
-from flask_cors import CORS
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_restless import APIManager 
-import os
+# Candidate model for SQLAlchemy database design class
+from init import db
 
-
-app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# can test on sqlite database first
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydb.sqlite')
-
-# switch to our mysql database when ready: (uncomment following line, comment out previous)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tpm:tpmpassword@aaj6jr738ea46y.cjnldv06yt97.us-east-2.rds.amazonaws.com:3306/ebdb'
-
-# don't set configs like this, do it like above
-# app.config['MYSQL_USER'] = 'tpm'
-# app.config['MYSQL_DATABASE'] = 'ebdb'
-# app.config['MYSQL_PASSWORD'] = 'tpmpassword'
-# app.config['MYSQL_HOST'] = 'aaj6jr738ea46y.cjnldv06yt97.us-east-2.rds.amazonaws.com'
-# app.config['MYSQL_PORT'] = '3306'
-
-# prevents a random warning
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-CORS(app)
-db = SQLAlchemy(app)
+cand_stock = db.Table('cand_stock',
+    db.Column('cand_crp_id', db.String(9), db.ForeignKey('candidate.cand_crp_id')),
+    db.Column('Symbol', db.String(10), db.ForeignKey('matchedstock.Symbol'))
+)
 
 class Candidate(db.Model):
     cand_crp_id = db.Column(db.String(9), primary_key=True)
@@ -40,9 +17,10 @@ class Candidate(db.Model):
     cand_image = db.Column(db.String(100), nullable=False, default='blank-profile3x2.png')
     contributions = db.relationship('Contribution', backref='recipient', lazy=True)
 
+    stocks_in_state = db.relationship('Matchedstock', secondary=cand_stock, backref=db.backref('cands_in_state', lazy='dynamic'))
+
     def __repr__(self):
         return f"Candidate('{self.cand_crp_id}', '{self.cand_firstname}', '{self.cand_lastname}')"
-
 
 class Contribution(db.Model):
     contrib_id = db.Column(db.String(150), primary_key=True)
@@ -54,22 +32,3 @@ class Contribution(db.Model):
 
     def __repr__(self):
         return f"Contribution('{self.org_name}', '{self.candidate_cand_crp_id}', {self.total})"
-
-
-
-
-
-
-# flask_restless automatically creates endpoints for the db.Model classes at /api/[class_name]:
-manager = APIManager(app, flask_sqlalchemy_db=db)
-manager.create_api(Candidate, results_per_page=16)
-manager.create_api(Contribution)
-
-
-
-if __name__ == '__main__':
-    app.run(port=8081, debug=True)
-
-
-
-
