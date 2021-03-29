89d020e5095922e2ef7792969f6d45d5a8394451
VaishnavBipin <vaishnavb88@gmail.com>
2021-03-29 13:40:05 -0500

    python unit tests

--- backend/tests.py
+++ backend/tests.py
@@ -1,48 +1,97 @@
 # all tests must begin with "test"
 from unittest import main, TestCase
-from CandidateDB import Candidate, Contribution
-from ContractDB import Contract
-from StockDB import Matchedstock
+import requests
 
-from init import db
-db.create_all()
 
 class TestBackend(TestCase):
+    def test_paging(self):
+        res = requests.get("https://api.thepoliticalmarket.tech/v1/candidate")
+        assert res.status_code == 200
+        assert res.json()["page"] == 1
+        res = requests.get("https://api.thepoliticalmarket.tech/v1/candidate?page=2")
+        assert res.status_code == 200
+        assert res.json()["page"] == 2
 
-    def test_1(self):
-        stock1 = Matchedstock(Symbol='AAPL', Full_Name='Apple', Last_Sale=0, Net_Change=0, Percentage_Change=0, Market_Cap=0, 
-                        Country='US', IPO_Year=0, Volume=0, Sector='sector', Industry=df['Industry'], Orgname=df['Orgname'], State='CA')
-        db.session.add(stock1)
-        stock2 = Matchedstock(Symbol='ABBV', Full_Name='Apple', Last_Sale=0, Net_Change=0, Percentage_Change=0, Market_Cap=0, 
-                        Country='US', IPO_Year=0, Volume=0, Sector='sector', Industry=df['Industry'], Orgname=df['Orgname'], State='CA')
-        db.session.add(stock2)
-        
+    def test_candidate(self):
+        res = requests.get("https://api.thepoliticalmarket.tech/v1/candidate")
+        assert res.status_code == 200
+        assert res.json()["num_results"] == 549
 
-        
+    def test_candidate_instance(self):
+        res = requests.get("https://api.thepoliticalmarket.tech/v1/candidate/N00000078")
+        assert res.status_code == 200
+        data = res.json()
+        assert data["cand_crp_id"] == "N00000078"
+        assert data["cand_firstname"] == "Carolyn"
+        assert data["cand_lastname"] == "Maloney"
+        assert (
+            data["cand_image"]
+            == "https://www.govtrack.us/static/legislator-photos/400251-200px.jpeg"
+        )
+        assert data["cand_office"] == "r"
+        assert data["cand_party"] == "D"
+        assert data["cand_state"] == "NY"
+        assert data["cycle"] == 2020
 
-        
-        
-        
+    def test_matchedstock(self):
+        res = requests.get("https://api.thepoliticalmarket.tech/v1/matchedstock")
+        assert res.status_code == 200
+        assert res.json()["num_results"] == 482
 
-    
+    def test_matchedstock_instance(self):
+        res = requests.get("https://api.thepoliticalmarket.tech/v1/matchedstock/aapl")
+        assert res.status_code == 200
+        data = res.json()
+        assert data["Country"] == "United Sta"
+        assert data["Full_Name"] == "Apple Inc. Common Stock"
+        assert data["IPO_Year"] == 1980
+        assert data["Industry"] == "Computer Manufacturing"
+        assert data["Last_Sale"] == 123.01
+        assert data["Market_Cap"] == 2147483647
+        assert data["Net_Change"] == -2.56
+        assert data["Orgname"] == "Apple Inc"
+        assert data["Percentage_Change"] == "-2.04%"
+        assert data["Sector"] == "Technology"
+        assert data["State"] == "CA"
+        assert data["Symbol"] == "AAPL"
+        assert data["Volume"] == 28163607
 
-if __name__=="__main__":
+    def test_contract(self):
+        res = requests.get("https://api.thepoliticalmarket.tech/v1/contract")
+        assert res.status_code == 200
+        assert res.json()["num_results"] == 4748
+
+    def test_contract_instance(self):
+        res = requests.get("https://api.thepoliticalmarket.tech/v1/contract/1")
+        assert res.status_code == 200
+        data = res.json()
+        assert (
+            data["contract_agencylogo"]
+            == "https://upload.wikimedia.org/wikipedia/commons/3/37/USDA_logo.png"
+        )
+        assert (
+            data["contract_award_description"]
+            == "200212!001205!2100!AB07  !USA COMMUNICATIONS-ELECTRONICS  !DAAB0702CJ204  !A!N! !N!                   !20020131!20031231!160386082!160386082!007321904!N!TEXAS INSTRUMENTS INCORPORATED!2501 W UNIVERSIT"
+        )
+        assert data["contract_award_id"] == "DAAB0702CJ204"
+        assert data["contract_currentval"] == 0.0
+        assert data["contract_date"] == "2002-01-31"
+        assert data["contract_naics"] == 334511
+        assert (
+            data["contract_naics_description"]
+            == "SEARCH, DETECTION, NAVIGATION, GUIDANCE, AERONAUTICAL, AND NAUTICAL SYSTEM AND INSTRUMENT MANUFACTUR"
+        )
+        assert data["contract_parentagency"] == "DEPARTMENT OF DEFENSE (DOD)"
+        assert data["contract_recipient"] == "TEXAS INSTRUMENTS INCORPORATED"
+        assert data["contract_recipient_address"] == "2501 W UNIVERSITY DR"
+        assert data["contract_recipient_district"] == 4
+        assert data["contract_sop"] == "TX"
+        assert (
+            data["contract_stateflag"]
+            == "https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg"
+        )
+        assert data["id"] == 1
+
+
+if __name__ == "__main__":
     main()
-
-# def pow2 (p) :
-#     return lambda v : v ** p
-
-# def test5 () :
-#     a = [2, 3, 4]
-#     m = map(pow2(2), a)
-#     assert list(m) == [4, 9, 16]
-#     assert list(m) == []
-
-# def test7 () :
-#     a     = [2, 3, 4]
-#     n     = [1]
-#     m     = map(lambda v : v ** next(iter(n)), a) # O(1)
-#     a    += [5]
-#     n[0]  = 2
-#     assert list(m) == [4, 9, 16, 25]
-#     assert list(m) == []
