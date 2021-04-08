c74d787c1b2ff3d5026b62571bcf2fce7a7e92af
kevinchenftw <kevinchenftw@gmail.com>
2021-04-07 22:06:56 -0500

    Search and filter UI

--- frontend/src/gov-models/Contracts.tsx
+++ frontend/src/gov-models/Contracts.tsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import {Pagination} from "@material-ui/core";
-import { DataGrid, 
-         GridRowsProp, 
-         GridColDef, 
-         GridCellParams,
-         GridSortModel,
-         GridSortModelParams } from '@material-ui/data-grid';
-
+import { Pagination } from "@material-ui/core";
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridCellParams,
+  GridSortModel,
+  GridSortModelParams
+} from '@material-ui/data-grid';
+import Dropdown from 'react-bootstrap/Dropdown'
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'Contract Page', width: 150,
+  {
+    field: 'id', headerName: 'Contract Page', width: 150,
     renderCell: (params: GridCellParams) => (
       <strong>
         <Button
@@ -22,52 +25,53 @@ const columns: GridColDef[] = [
           Open
         </Button>
       </strong>
-    ),},
-    { field: 'contract_award_id', headerName: 'Award ID', width: 200 },
-    { field: 'contract_recipient', headerName: 'Recipient', width: 200},
-    {
-      field: 'contract_currentval', headerName: 'Contract Value',
-      type: 'number',
-      width: 160,
-    },
-    {
-      field: 'contract_date',
-      headerName: 'Award Date',
-      width: 160,
-    },
-    {
-      field: 'contract_naics', headerName: 'NAICS',
-      type: 'string',
-      width: 150,
-    },
-    {
-      field: 'contract_sop',
-      headerName: 'State',
-      width: 160,
-    },
-    {
-      field: 'contract_recipient_district',
-      headerName: 'Congressional District',
-      width: 140
-    }
-  ];
+    ),
+  },
+  { field: 'contract_award_id', headerName: 'Award ID', width: 200 },
+  { field: 'contract_recipient', headerName: 'Recipient', width: 200 },
+  {
+    field: 'contract_currentval', headerName: 'Contract Value',
+    type: 'number',
+    width: 160,
+  },
+  {
+    field: 'contract_date',
+    headerName: 'Award Date',
+    width: 160,
+  },
+  {
+    field: 'contract_naics', headerName: 'NAICS',
+    type: 'string',
+    width: 150,
+  },
+  {
+    field: 'contract_sop',
+    headerName: 'State',
+    width: 160,
+  },
+  {
+    field: 'contract_recipient_district',
+    headerName: 'Congressional District',
+    width: 140
+  }
+];
 
 const url = "https://api.thepoliticalmarket.tech/v1/contract"
 
-function Contracts(){
-  
+function Contracts() {
+
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<GridRowsProp>([] as GridRowsProp);
   const [page, setPage] = useState(1);
   const [numPages, setNumPages] = useState(0)
   const [searchTerm, setSearchTerm] = useState("");
-  const [sortCol, setSortCol] = useState<GridSortModel>([{field:'id', sort:'asc'}])
+  const [sortCol, setSortCol] = useState<GridSortModel>([{ field: 'id', sort: 'asc' }])
 
   useEffect(() => {
-    let toFetch = url+`?page=${page}`;
+    let toFetch = url + `?page=${page}`;
     if (sortCol.length !== 0) {
-      let query = {field: sortCol[0].field, direction:sortCol[0].sort};
-      toFetch = toFetch+`&q={"order_by":[${JSON.stringify(query)}]}`
+      let query = { field: sortCol[0].field, direction: sortCol[0].sort };
+      toFetch = toFetch + `&q={"order_by":[${JSON.stringify(query)}]}`
     }
     fetch(toFetch, {})
       .then((res) => res.json())
@@ -88,40 +92,54 @@ function Contracts(){
 
   if (isLoading) {
     return <h2>Loading...</h2>
-  }  
+  }
   return (
     <> {
       <div>
+        <div style = {{textAlign: "right"}}>
         <input
           type="text"
           placeholder="Search..."
-          onChange = {(event) => {
+          onChange={(event) => {
             setSearchTerm(event.target.value)
+            console.log(event.target.value)
           }}></input>
-      <div style={{ height: 800, width: '100%' }}>
-        <DataGrid
-          rows={data} 
-          columns={columns} 
-          pageSize={10}
-          hideFooterPagination={true}
-          checkboxSelection 
-          sortingMode="server"
-          onSortModelChange={handleSort}
-        />
-      </div>
-      <h5>
-      </h5>
-      <Pagination 
-          count = {numPages}
-          onChange = {(event, page) => setPage(page)}
+          </div>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Filters
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">States</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <div style={{ height: 800, width: '100%' }}>
+          <DataGrid
+            rows={data}
+            columns={columns}
+            pageSize={10}
+            hideFooterPagination={true}
+            checkboxSelection
+            sortingMode="server"
+            onSortModelChange={handleSort}
+          />
+        </div>
+        <h5>
+        </h5>
+        <Pagination
+          count={numPages}
+          onChange={(event, page) => setPage(page)}
           showFirstButton
           showLastButton
-          variant = "outlined"
+          variant="outlined"
           color="primary"
           size="large"
-      />
+        />
       </div>
-      } </>
+    } </>
   );
 }
 export default Contracts
