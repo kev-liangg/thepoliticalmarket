eabc34c0b23eacb5b372a35e0452aa444173f220
Anisha <bluestar225@gmail.com>
2021-04-14 22:15:07 -0500

    cleaned up guitests

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -9,7 +9,6 @@ class GUIHarness(unittest.TestCase):
     def test_url(self):
         browser = Browser('chrome')
         browser.visit('https://www.thepoliticalmarket.tech/')
-        title = browser.title
         self.assertTrue(browser.is_text_present("The Political Market"))
     def test_navBarLinks(self):
         browser = Browser('chrome')
@@ -19,22 +18,18 @@ class GUIHarness(unittest.TestCase):
     def test_contractsTest(self):
         browser = Browser('chrome')
         browser.visit('https://www.thepoliticalmarket.tech/Contracts')
-        text = browser.find_by_text('Contract Page')
         self.assertTrue(browser.is_text_present("Contract Page")) 
     def test_politiciansTest(self):
         browser = Browser('chrome')
         browser.visit('https://www.thepoliticalmarket.tech/CampFin')
-        text = browser.find_by_text('OpenSecrets ID')
         self.assertTrue(browser.is_text_present("OpenSecrets ID")) 
     def test_stocksTest(self):
         browser = Browser('chrome')
         browser.visit('https://www.thepoliticalmarket.tech/Stocks')
-        text = browser.find_by_text('Symbol')
         self.assertTrue(browser.is_text_present("Symbol")) 
     def test_aboutPage(self):
         browser = Browser('chrome')
         browser.visit('https://www.thepoliticalmarket.tech/about')
-        text = browser.find_by_text('Team Members')
         self.assertTrue(browser.is_text_present("Team Members"))
 
 
