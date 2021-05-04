468cd20c4734f53d2140ae6d028f21818852f843
Anisha <bluestar225@gmail.com>
2021-05-04 08:15:33 -0500

    fix formatting

--- frontend/src/Visualizations/DoggieDBVisuals.js
+++ frontend/src/Visualizations/DoggieDBVisuals.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import DoggiePieChart from './DoggiePieChart';
 import ShelterBubbleChart from './ShelterBubbleChart';
-import { Container } from '@material-ui/core';
+import DogBarChart from './DogBarChart';
 
 
 function DoggieDBVisuals() {
     
     return (
         <div>
-            <div className="col-lg-6" style={{height: '1000px', width: '1000px','textAlign':'center'}}>
+            <div className="col-lg-6" style={{height: '1000px', width: '1000px','textAlign':'center', top: "100px"}}>
                 <h3> Breed Classifications</h3>
                 <DoggiePieChart/>
             </div>
-            <div style={{ height: '1000px', width: '1000px','textAlign':'center'}}>
+            <div className="col-lg-6" style={{ height: '1000px', width: '1000px','textAlign':'center', top: "100px"}}>
                 <h3> Shelters per State</h3>
                 <ShelterBubbleChart />
+            </div>
+            <div className="col-lg-6" style={{height: '1000px', width: '1000px','textAlign':'center', top: "200px"}}>
+                <h3> Dogs by Gender and Age</h3>
+                <DogBarChart />
             </div>
         </div>
        
