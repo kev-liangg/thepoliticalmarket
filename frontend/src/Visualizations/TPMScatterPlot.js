a7fdabf1a0507531869613b0e7a6e149f03d8f20
VaishnavBipin <vaishnavb88@gmail.com>
2021-05-04 04:48:51 -0500

    tpm visuals

--- frontend/src/Visualizations/TPMScatterPlot.js
+++ frontend/src/Visualizations/TPMScatterPlot.js
@@ -1,29 +1,27 @@
 import React, { useState, useEffect } from "react";
-import {ScatterChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Scatter} from "recharts";
+import {scatterData} from "./stocksScatterData";
+import {ScatterChart, CartesianGrid, XAxis, YAxis, ZAxis, Tooltip, Legend, Scatter} from "recharts";
 
 function TPMScatterPlot() {
-    const [data, setData] = useState();
-
-    // useEffect(() => {
-    //     fetch(`https://api.thepoliticalmarket.tech/v1/candidate?page=${page}`, {})
-    //       .then((res) => res.json())
-    //       .then((response) => {
-    //         setData(response);
-    //         setIsLoading(false);
-    //       })
-    //       .catch((error) => console.log(error));
-    // }, []);
     
-    // if (isLoading) {
-    //     return <h2>Loading...</h2>
-    // }
     return (
         <div>
-            <ScatterChart width='800px' height='600px'>
+            <h3 style={{'textAlign':'center', 'color':'#4a50f0'}}>Stock Volume vs Market Capitalization</h3>
+            <ScatterChart 
+                width={1200} 
+                height={500}
+                margin={{
+                    top: 10,
+                    left: 100,
+                    right: 30
+                }}
+            >
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="x" name="market cap" unit="$ (millions)" />
-                <YAxis dataKey="y" name="volume" unit="shares" />
+                <XAxis dataKey="Market_Cap" type="number" name="market cap" domain={[0, 2200000000000]} scale="auto"/>
+                <YAxis dataKey="Volume" type="number" name="volume" domain={[0,54000000]}/>
+                <ZAxis dataKey="Symbol" type="string" />
                 <Tooltip cursor={{strokeDasharray:"2 2"}} />
+                <Scatter name="stocks" data={scatterData} fill="#4a50f0" />
             </ScatterChart>
         </div>
     )
--- frontend/src/Visualizations/TPMVisuals.js
+++ frontend/src/Visualizations/TPMVisuals.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import TPMScatterPlot from "./TPMScatterPlot";
+import TPMBarGraph from "./TPMBarGraph";
+import TPMPieGraph from "./TPMPieGraph";
 
 function TPMVisuals() {
     
     return (
         <div>
-            
+            <h1 style={{'textAlign':'center'}}>Visualizations of Our Data</h1>
+            <br></br>
+            <br></br>
+            <TPMBarGraph />
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <TPMScatterPlot />
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <TPMPieGraph />
+            <br></br>
+            <br></br>
+
         </div>
     )
 }
